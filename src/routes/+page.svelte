f714fcfac8559c3ba8794c3afff1f6565e3282e9
Omaira85 <ladyotavo@gmail.com>
2023-05-30 17:41:56 -0500

    feat: estructura inicial frontend

---
+++ src/routes/+page.svelte
@@ -0,0 +1,102 @@
+<script>
+  import { onMount } from 'svelte';
+    import { text } from 'svelte/internal';
+  var mensaje = ''
+  var resultados = []
+
+  var requestSearch = {
+    text: '',
+    tipo: '',
+    anio: '',
+    fecha_inicio:'',
+    fecha_fin: ''
+  }
+
+  onMount(async () => {
+		var response = await fetch('http://localhost:5000/mensaje')
+    mensaje = await response.text()
+
+    response = await fetch('http://localhost:5000/resultados')
+    resultados = await response.json()
+	});
+
+  async function search(){
+    var response = await fetch('http://localhost:5000/search', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(requestSearch)
+    })
+    resultados = await response.json()
+  }
+
+
+</script>
+<div class="container">
+  <h1>{mensaje}</h1>
+  <main class="form-signin w-100 m-auto">
+    <form>
+      <img class="mb-4" src="/logo_corte.png" alt="" width="72" height="57">
+      <div class="title">
+        <h1 class="h3 mb-3 fw-normal">Buscador de relatoría</h1>
+      </div>
+      <div class="form-group">
+        <input
+          type="text"
+          class="form-control form-control-lg"
+          placeholder="Buscar..."
+          bind:value='{requestSearch.text}'
+        />
+      </div>
+      <div class="flex-container">
+      <div class="form-group">
+        <label for="tipo">Tipo:</label>
+        <select class="form-control" id="tipo" bind:value="{requestSearch.tipo}">
+          <option value="Constitucional">Constitucional</option> 
+          <option value="Auto">Auto</option>
+          <option value="Tutela">Tutela</option>
+        </select>
+      </div>
+      <div class="form-group">
+        <label for="anio">Año:</label>
+        <select class="form-control" id="anio" bind:value="{requestSearch.anio}">
+          <option value="2023">2023</option>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
+          <option value="2018">2018</option>
+        </select>
+      </div>
+      <div class="form-group">
+        <label for="fecha_inicio">Fecha inicio:</label>
+        <input type="date" class="form-control" id="fecha_inicio" bind:value="{requestSearch.fecha_inicio}"/>
+      </div>
+      <div class="form-group">
+        <label for="fecha_fin">Fecha fin:</label>
+        <input type="date" class="form-control" id="fecha_fin" bind:value="{requestSearch.fecha_fin}"/>
+      </div>
+    </div>
+      <div class="checkbox mb-3">
+      </div>
+      <button class="w-100 btn btn-lg btn-primary" type="button" on:click={search}>Buscar</button>
+      <p class="mt-5 mb-3 text-body-secondary">&copy; 2017–2023</p>
+    </form>
+  </main>  
+  <div class="row justify-content-center">
+    <div class="col-md-6">
+    </div>
+    <div class="col-md-6" id="form-container">
+      <ul>
+        {#each resultados as item}
+          <li>
+            <strong>{item.title}</strong>
+            <br>
+            <p>{item.description}</p>
+          </li>
+        {/each}
+      </ul>
+    </div>
+  </div>
+</div>
